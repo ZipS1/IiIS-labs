1a761b934fb95050d1503b11a8aa10b0cb7bb37d
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2024-05-13 17:42:18 +0300

    Formula changes and remove profiler

--- lab09/Controller.py
+++ lab09/Controller.py
@@ -7,13 +7,14 @@ EDGE_OFFSET = 20
 POINT_RADIUS = 5
 HEAT_BOUND = 0.1
 POINT_POWER = 50
-DEBUG_SHOW_TOWER_RADIUS = True #TODO
+DEBUG_SHOW_TOWER_RADIUS = True  # TODO
+
 
 class Controller:
     def __init__(self, N):
         self.model = Model(N, (WIDTH, HEIGHT), EDGE_OFFSET)
         self.view = View((WIDTH, HEIGHT))
-        self.pixels = [[(0,0,0) for i in range(WIDTH)] for j in range(HEIGHT)]
+        self.pixels = [[(0, 0, 0) for i in range(WIDTH)] for j in range(HEIGHT)]
         self.heat_map = [[0 for i in range(WIDTH)] for row in range(HEIGHT)]
         self.point_coords = []
 
@@ -26,29 +27,23 @@ class Controller:
         self.point_coords = self.model.get_point_coords()
 
         self._evaluate_heat_map()
-        with open("heat_map.txt", "w") as f:
-            for row in self.heat_map:
-                for val in row:
-                    f.write(str(val) + " ")
-                f.write("\n")
-                                
         self._adjust_map_with_heat_bound(HEAT_BOUND)
 
         for point in self.point_coords:
             self._set_point(point[0], point[1])
 
     def _set_point(self, x, y):
-        self.pixels[y][x] = (255,0,0)
+        self.pixels[y][x] = (255, 0, 0)
         for t in np.arange(0, np.pi, 0.05):
             y_diff = int(np.sin(t) * POINT_RADIUS)
             x_diff = int(np.cos(t) * POINT_RADIUS)
 
             for i in range(x_diff):
                 for j in range(y_diff):
-                    self.pixels[y + i][x + j] = (255,0,0)
-                    self.pixels[y + i][x - j] = (255,0,0)
-                    self.pixels[y - i][x + j] = (255,0,0)
-                    self.pixels[y - i][x - j] = (255,0,0)
+                    self.pixels[y + i][x + j] = (255, 0, 0)
+                    self.pixels[y + i][x - j] = (255, 0, 0)
+                    self.pixels[y - i][x + j] = (255, 0, 0)
+                    self.pixels[y - i][x - j] = (255, 0, 0)
 
     def _evaluate_heat_map(self):
         for point in self.point_coords:
@@ -59,24 +54,29 @@ class Controller:
 
                 for i in range(x_diff):
                     for j in range(y_diff):
-                        for x2, y2 in ((x1 + i, y1 + j), (x1 - i, y1 + j), (x1 + i, y1 - j), (x1 - i, y1 - j)):
+                        for x2, y2 in (
+                            (x1 + i, y1 + j),
+                            (x1 - i, y1 + j),
+                            (x1 + i, y1 - j),
+                            (x1 - i, y1 - j),
+                        ):
                             if x2 >= 0 and x2 < WIDTH and y2 >= 0 and y2 < HEIGHT:
                                 distance = self._get_distance(x1, y1, x2, y2)
                                 value = self.heat_map[y2][x2]
                                 if distance == 0:
                                     value = 1
                                 elif value < 1:
-                                    value += 1 - distance / POINT_POWER # (3 * POINT_POWER**2) / (distance**4)
+                                    value += 1 - distance / POINT_POWER
                                     if value > 1:
                                         value = 1
-                                                                                                                                                
+
                                 self.heat_map[y2][x2] = value
-    
+
     def _adjust_map_with_heat_bound(self, heat_bound):
         for i, row in enumerate(self.heat_map):
             for j, value in enumerate(row):
                 if value > heat_bound:
-                    self.pixels[i][j] = (0,255,0)
+                    self.pixels[i][j] = (0, 255, 0)
 
     def _get_distance(self, x1, y1, x2, y2):
-        return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
--- lab09/main.py
+++ lab09/main.py
@@ -1,22 +1,18 @@
-'''
+"""
 14. Сотовые вышки. Известны координаты на местности N сотовых вышек (на рисунке красные точки; N=100). 
 Сигнал от вышек ослабевает обратноропорционально расстоянию от вышки. При этом сигналы от всех вышек складываются.
 Зона уверенной связи имеет место в том случае, когда сигнал больше 0,1 от максимального значения сигнала –
 на рисунке эта зона залита зеленым цветом.
-'''
+"""
+
 from Controller import Controller
-from pyinstrument import Profiler
+
 
 def main():
-    profiler = Profiler()
-    profiler.start()
-
-    N = 100
+    N = 1
     controller = Controller(N)
     controller.run()
 
-    profiler.stop()
-    profiler.open_in_browser()
 
 if __name__ == "__main__":
     main()
